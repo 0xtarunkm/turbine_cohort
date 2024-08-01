367ad46250db30566d60443ff3c2f901c7ffa19c
tarunclub <tarunkm__@outlook.com>
2024-08-01 20:45:36 +0530

    spl-token v1 done

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -1,26 +1,34 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+} from '@metaplex-foundation/umi';
+import {
+  createNft,
+  mplTokenMetadata,
+} from '@metaplex-foundation/mpl-token-metadata';
 
-import wallet from "../wba-wallet.json"
-import base58 from "bs58";
+import wallet from '../wba-wallet.json';
+import base58 from 'bs58';
 
-const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const RPC_ENDPOINT = 'https://api.devnet.solana.com';
 const umi = createUmi(RPC_ENDPOINT);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+  // let tx = ???
+  // let result = await tx.sendAndConfirm(umi);
+  // const signature = base58.encode(result.signature);
 
-    console.log("Mint Address: ", mint.publicKey);
+  // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+
+  console.log('Mint Address: ', mint.publicKey);
 })();
--- ts/prereqs/airdrop.ts
+++ ts/prereqs/airdrop.ts
@@ -1,18 +1,21 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
-import wallet from "./dev-wallet.json"
+import { Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import wallet from './dev-wallet.json';
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection to claim 2 devnet SOL tokens
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection('https://api.devnet.solana.com');
 
 (async () => {
-    try {
-        const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+  try {
+    const txhash = await connection.requestAirdrop(
+      keypair.publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
     console.log(`Success! Check out your TX here: 
     https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
